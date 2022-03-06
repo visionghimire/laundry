9d576d5ec5208f50898932728640553d6ea902d2
Vision <vision.ghimire96@gmail.com>
2022-03-06 15:10:38 +0545

    laundry/edit

--- lms.sql
+++ lms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 04, 2022 at 05:58 PM
+-- Generation Time: Mar 06, 2022 at 09:17 AM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -52,7 +52,6 @@ CREATE TABLE `booking` (
 --
 
 INSERT INTO `booking` (`id`, `type`, `service_type`, `unit`, `price`, `total`, `trackingcode`, `fullname`, `email`, `phone`, `pickupdate`, `timeslot`, `address`, `city`, `status`, `created_at`) VALUES
-(1, 1, 'Wash and Fold', 2, 0, 0, '', 'khagendra niroula', 'admin@admin.com', '9842789482', '2021-11-24', '9AM-12PM', 'anamnagar', 'ktm', 4, '2021-11-20 12:47:17'),
 (2, 1, 'Wash and Fold', 2, 30, 60, '16373926111', 'khagendra niroula', 'admin@admin.com', '9842789482', '2021-11-23', '9AM-12PM', 'anamnagar', 'ktm', 4, '2021-11-20 13:01:51'),
 (3, 3, 'Wash and Fold', 2, 25, 50, '16373926742', 'khagendra niroula', 'admin@admin.com', '9842789482', '2021-11-25', '9AM-12PM', 'anamnagar', 'ktm', 2, '2021-11-20 13:02:54'),
 (4, 1, 'Wash and Fold', 2, 30, 60, '16373927633', 'khagendra niroula', 'admin@admin.com', '9842789482', '2021-11-27', '9AM-12PM', 'anamnagar', 'ktm', 3, '2021-11-20 13:04:23'),
@@ -61,7 +60,26 @@ INSERT INTO `booking` (`id`, `type`, `service_type`, `unit`, `price`, `total`, `
 (7, 4, 'Wash and Fold', 10.3, 100, 1030, '16374093496', 'asfasdf', 'admin@admin.com', '343333', '2021-11-24', '9AM-12PM', 'sdfsdf', 'sdfsd', 1, '2021-11-20 17:40:49'),
 (8, 3, 'Wash and Fold', 5, 25, 125, '16375547307', 'Mel Pinto', 'admin@admin.com', '9668563256', '2021-11-23', '12PM-3PM', 'philippines', 'manila', 4, '2021-11-22 10:03:50'),
 (9, 1, 'Wash and Fold', 2, 30, 60, '16375557358', 'Lance Asturis', 'admin@admin.com', '9865475632', '2021-11-30', '3PM-6PM', 'Pearl Manila', 'Manila', 4, '2021-11-22 10:20:35'),
-(10, 3, 'Wash and Iron', 6, 25, 150, '16376023549', 'Kalson Karki', 'admin@admin.com', '9845632154', '2021-11-22', '12PM-3PM', 'kapan', 'kathmandu', 3, '2021-11-22 23:17:36');
+(10, 3, 'Wash and Iron', 6, 25, 150, '16376023549', 'Kalson Karki', 'admin@admin.com', '9845632154', '2021-11-22', '12PM-3PM', 'kapan', 'kathmandu', 3, '2021-11-22 23:17:36'),
+(11, 4, 'Wash and Fold', 5, 100, 500, '16417909729', 'Lucky Trevor', 'trevor@gmail.com', '112233445566', '2022-01-11', '3PM-6PM', '268 Manila', 'Manila', 4, '2022-01-10 10:47:53'),
+(12, 3, 'Wash and Fold', 7, 25, 175, '164223122210', 'Hello', 'hello@gmail.com', '546558569', '2022-01-15', '3PM-6PM', 'santa cruize', 'manila', 4, '2022-01-15 13:05:23'),
+(13, 1, 'Wash and Fold', 5, 30, 150, '164545070611', 'Vision', 'vision@gmail.com', '98545522145', '2022-02-22', '12PM-3PM', 'Pearl Manila', 'Manila', 1, '2022-02-21 19:23:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `contact_number` varchar(50) NOT NULL,
+  `clock_in` varchar(50) NOT NULL,
+  `clock_out` varchar(50) NOT NULL,
+  `availability` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -86,7 +104,8 @@ CREATE TABLE `inventory` (
 INSERT INTO `inventory` (`id`, `supply_id`, `in_qty`, `price`, `used_qty`, `remaining_qty`, `date_created`) VALUES
 (5, 1, 20, 1200, 15, 5, '2022-01-04 19:46:32'),
 (6, 2, 25, 3500, NULL, 25, '2022-01-04 19:47:16'),
-(7, 3, 12, 1200, NULL, 12, '2022-01-04 20:01:53');
+(7, 3, 12, 1200, NULL, 12, '2022-01-04 20:01:53'),
+(8, 4, 50, 100, 5, 45, '2022-01-09 18:26:19');
 
 -- --------------------------------------------------------
 
@@ -178,7 +197,30 @@ CREATE TABLE `stock_list` (
 INSERT INTO `stock_list` (`id`, `name`, `price`) VALUES
 (1, 'Fabric Detergent', '2.5'),
 (2, 'Fabric Conditioner', '3'),
-(3, 'Baking Soda', '1.2');
+(3, 'Baking Soda', '1.2'),
+(4, 'Downy', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_slot`
+--
+
+CREATE TABLE `time_slot` (
+  `id` int(10) NOT NULL,
+  `slot` varchar(100) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `time_slot`
+--
+
+INSERT INTO `time_slot` (`id`, `slot`, `status`) VALUES
+(1, '8AM-9AM', 0),
+(2, '9AM-11AM', 1),
+(3, '12PM-3PM', 1),
+(4, '5PM-7PM', 0);
 
 -- --------------------------------------------------------
 
@@ -241,6 +283,12 @@ ALTER TABLE `stock_list`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `time_slot`
+--
+ALTER TABLE `time_slot`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -254,13 +302,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `booking`
 --
 ALTER TABLE `booking`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `inventory`
 --
 ALTER TABLE `inventory`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `laundry_categories`
@@ -284,7 +332,13 @@ ALTER TABLE `laundry_list`
 -- AUTO_INCREMENT for table `stock_list`
 --
 ALTER TABLE `stock_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `time_slot`
+--
+ALTER TABLE `time_slot`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
